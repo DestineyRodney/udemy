3405daf1f8945b114eddac40c5d409623b9e5abb
Destiney Rodney <destiney.rodney01@gmail.com>
2022-02-15 19:58:34 -0600

    feat: calc functionaility

--- assets/scripts/calc.js
+++ assets/scripts/calc.js
@@ -1,1 +1,7 @@
+
 alert('this works');
+let currentResult = 0;
+
+currentResult = (10 * 3) + (currentResult + 5);
+
+outputResult(currentResult, 'hi');
---
+++ assignment1/assignment.js
@@ -0,0 +1,11 @@
+let userInput = 5;
+let result;
+result = userInput + 18;
+alert(result) //23
+result = result - 2; // 21
+alert(result)
+result = result * 2; // 42
+alert(result)
+result = result / 2;// 21
+alert(userInput);
+alert(result)
