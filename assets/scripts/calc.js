8b53c64c662ce030182f3edb4bf9ced252dae160
Destiney Rodney <destiney.rodney01@gmail.com>
2022-02-17 23:23:22 -0600

    feat: completed module 2. Added entry log function

--- assets/scripts/calc.js
+++ assets/scripts/calc.js
@@ -1,12 +1,25 @@
 // alert('this works');
 let currentResult = 0;
+let entries = [];
 
+function logEntries(operation, enteredNum, prevResult, currentResult){
+    entryLog = {
+        operation: operation,
+        prevResult: prevResult,
+        number: enteredNum,
+        finalResult: currentResult
+    };
+    entries.push(entryLog);
+    console.log(entries);
+}
 // currentResult = (10 * 3) + (currentResult + 5);
 function add (){
     const userNum = parseInt(userInput.value);
+    const initialResult = currentResult;
     const calcDescription = `${currentResult} + ${userNum}`; // put here so we get current result and not the one after user input
     currentResult = currentResult + userNum;  // .value gives the string of the number as text and parse gives the num as actual number
     outputResult(currentResult, calcDescription);
+    logEntries('Add', userNum, initialResult, currentResult);
 
 }
 
@@ -22,18 +35,22 @@ adbtn.addEventListener('click', add); //takes 2 arguments (string) and second pa
 
 function subtract (){
     const userNum = parseInt(userInput.value);
+    const initialResult = currentResult;
     const calcDescription = `${currentResult} - ${userNum}`;
     currentResult = currentResult - userNum;
     outputResult(currentResult, calcDescription );
+    logEntries('Subtract', userNum, initialResult, currentResult)
 }
 subtractbtn = document.getElementById('btn-subtract');
 subtractbtn.addEventListener('click', subtract);
 
 function mult (){
     const userNum = parseInt(userInput.value);
+    const initialResult = currentResult;
     const calcDescription = `${currentResult} * ${userNum}`;
     currentResult = currentResult * userNum;
     outputResult(currentResult, calcDescription);
+    logEntries('multiply', userNum, initialResult, currentResult);
 }
 
 multbtn = document.getElementById('btn-multiply');
@@ -41,9 +58,11 @@ multbtn.addEventListener('click', mult);
 
 function divide (){
     const userNum = parseInt(userInput.value);
+    const initialResult = currentResult;
     const calcDescription = `${currentResult} / ${userNum}`;
     currentResult = currentResult /userNum;
     outputResult(currentResult, calcDescription);
+    logEntries('divide', userNum, initialResult, currentResult);
 }
 dividebtn = document.getElementById('btn-divide');
 dividebtn.addEventListener('click', divide);
--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -12,3 +12,4 @@ function outputResult(result, text) {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
 }
+
